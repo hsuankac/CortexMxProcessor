513ddaf07052929872cecb54358a5a2e14553017
hsuankai.chang <hsuankai.chang@driveseres.com>
2022-11-23 12:05:38 -0800

    Finish review to section6 GCC inline assemly coding

--- Workspace/004Inline_1/Src/main.c
+++ Workspace/004Inline_1/Src/main.c
@@ -24,35 +24,28 @@
 
 int main(void)
 {
-#if 1
+#if 0
 	__asm volatile("LDR R1, =#0x20001000");
 	__asm volatile("LDR R2, =#0x20001004");
-	// LDR = LOAD
-	__asm volatile("LDR R0, [R1]");
-	__asm volatile("LDR R1, [R2]");
-	__asm volatile("ADD R0, R0, R1");
-
-	// STR = STORE
-	__asm volatile("STR R0, [R1]");
+	__asm volatile("LDR R0,[R1]");
+	__asm volatile("LDR R1,[R2]");
+	__asm volatile("ADD R0,R0,R1");
+	__asm volatile("STR R0,[R2]");
 #endif
-#if 0
-	// example1: move the c variable val to arm register r0
+
+	// move the content of c variable val to register R0
 	int val = 50;
 	__asm volatile("MOV R0, %0": :"r"(val));
 
-	// example2: read CONTROL register to control_reg, '=' constraint modifier is write-only
+	// move the content of CONTROL  register to c variable control_reg
 	int control_reg;
 	__asm volatile("MRS %0, CONTROL": "=r"(control_reg));
 
-	// example3: copy the content of 'C' variable var1 and var2
-	int var1=10, var2;
-	__asm volatile("MOV %0, %1": "=r"(var2): "r"(var1));
-
-	// example4: copy the contents of a pointer into another variable
+	// copy the content of a pointer into another variable
 	int p1, *p2;
 	p2 = (int*)0x20000008;
 	__asm volatile("LDR %0, [%1]": "=r"(p1): "r"(p2));
-#endif
+
     /* Loop forever */
 	for(;;);
 
